41a159ba4fe60382583c95688e2c1b03f9eb5a39
Ade-yanju <adeyanjuolamilekan080@gmail.com>
2026-01-25 15:50:17 +0100

    gg

--- src/routes/payment.routes.js
+++ src/routes/payment.routes.js
@@ -1,14 +1,14 @@
 import express from "express";
 import {
   initiatePayment,
-  getPaymentStatus,
+  //getPaymentStatus,
   paymentCallback,
 } from "../controllers/payment.controller.js";
 
 const router = express.Router();
 
 router.post("/initiate", initiatePayment);
-router.get("/status/:reference", getPaymentStatus);
+//router.get("/status/:reference", getPaymentStatus);
 router.get("/callback", paymentCallback);
 
 export default router;
